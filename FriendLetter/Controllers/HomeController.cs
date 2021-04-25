a0af153dd532bc9bcf47b2ee9023d206c52febd3
Sofia Acosta <sofiaacostarascon@gmail.com>
2021-04-25 18:30:22 -0500

    Generate custom postcard for their friends

--- FriendLetter/Controllers/HomeController.cs
+++ FriendLetter/Controllers/HomeController.cs
@@ -17,7 +17,18 @@ namespace FriendLetter.Controllers
       LetterVariable myLetterVariable = new LetterVariable();
       myLetterVariable.Recipient = "Lina";
       myLetterVariable.Sender = "Jasmine";
+      return View(myLetterVariable);}
+
+    [Route("/form")]
+    public ActionResult Form() { return View(); }
+
+    [Route("/postcard")]
+    public ActionResult Postcard(string recipient, string sender)
+    {
+      LetterVariable myLetterVariable = new LetterVariable();
+      myLetterVariable.Recipient = recipient;
+      myLetterVariable.Sender = sender;
       return View(myLetterVariable);
     }
+    }  
   }
-}
